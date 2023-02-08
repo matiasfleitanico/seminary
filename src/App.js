fa6637f5c4991b9e87647f61bbdf99c61a513f64
Matias Fleita <matiasfleitanico@gmail.com>
2023-02-08 19:34:13 -0300

    all new format is up

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Register from "./components/Register";
-import Identity from "./components/Identity";
+import IdentityOne from "./components/Identity/IdentityOne";
+import IdentityTwo from "./components/Identity/IdentityTwo";
+import IdentityThree from "./components/Identity/IdentityThree";
+import IdentityFourth from "./components/Identity/IdentityFourth"; 
+import IdentityFiveth from "./components/Identity/IdentityFiveth"; 
+import NewWineOne from "./components/NewWine/NewWineOne";
+import NewWineTwo from "./components/NewWine/NewWineTwo";
+import NewWineThree from "./components/NewWine/NewWineThree";
+import NewWineFourth from "./components/NewWine/NewWineFourth"; 
+import NewWineFiveth from "./components/NewWine/NewWineFiveth"; 
+import NewWineSixth from "./components/NewWine/NewWineSixth"; 
+import NewWine from "./components/NewWinBase";
 import { AuthProvider } from "./context/authContext"
 
 
@@ -18,11 +29,61 @@ function App() {
           } />
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
-        <Route path="/identidad" element={
+
+
+        <Route path="/identidad/1" element={
           <ProtectedRoute>
-            <Identity/>
+            <IdentityOne/>
+          </ProtectedRoute>} />
+          <Route path="/identidad/2" element={
+          <ProtectedRoute>
+            <IdentityTwo/>
+          </ProtectedRoute>} />
+          <Route path="/identidad/3" element={
+          <ProtectedRoute>
+            <IdentityThree/>
+          </ProtectedRoute>} />
+          <Route path="/identidad/4" element={
+          <ProtectedRoute>
+            <IdentityFourth/>
+          </ProtectedRoute>} />
+          <Route path="/identidad/5" element={
+          <ProtectedRoute>
+            <IdentityFiveth/>
+          </ProtectedRoute>} />
+
+          <Route path="/vinonuevo/1" element={
+          <ProtectedRoute>
+            <NewWineOne/>
+          </ProtectedRoute>} />
+          <Route path="/vinonuevo/2" element={
+          <ProtectedRoute>
+            <NewWineTwo/>
+          </ProtectedRoute>} />
+          <Route path="/vinonuevo/3" element={
+          <ProtectedRoute>
+            <NewWineThree/>
+          </ProtectedRoute>} />
+          <Route path="/vinonuevo/4" element={
+          <ProtectedRoute>
+            <NewWineFourth/>
+          </ProtectedRoute>} />
+          <Route path="/vinonuevo/5" element={
+          <ProtectedRoute>
+            <NewWineFiveth/>
+          </ProtectedRoute>} />
+          <Route path="/vinonuevo/6" element={
+          <ProtectedRoute>
+            <NewWineSixth/>
+          </ProtectedRoute>} />
+
+
+        <Route path="/vino-nuevo" element={
+          <ProtectedRoute>
+            <NewWine/>
           </ProtectedRoute>} />
       </Routes>
+
     </AuthProvider>
   );
 }
---
+++ src/components/base.js
@@ -0,0 +1,141 @@
+import { useState } from "react";
+import { useAuth } from "../context/authContext";
+import { useNavigate } from "react-router-dom";
+import { usuario } from "../Users";
+import React from "react";
+
+export  default function Identidad() {
+    const {user} = useAuth();
+    const navigate = useNavigate()
+    var user_id;
+
+    var main = {
+        display: "grid",
+        gridTemplateColumns: "15% 85%"
+    }
+    var sidebar = {
+        display: "grid",
+        alignItems: "center",
+        justifyItems: "center",
+        gridTemplateColumns: "1fr",
+        padding: "15px",
+        gridGap: "30px",
+        backgroundColor: "white",
+        width: "100px",
+        height: "800px",
+        margin: "15px",
+        justifySelf: "center",
+        alignSelf: "start",
+        borderRadius: "10px",
+        boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px"
+    }
+
+    const customStyle = {
+        display: "grid",
+        alignItems: "center",
+        justifyItems: "center",
+        gridTemplateColumns: "1fr",
+        padding: "15px",
+        gridGap: "30px",
+        backgroundColor: "white",
+        width: "auto",
+        height: "100%",
+        margin: "15px",
+        justifySelf: "center",
+        alignSelf: "center",
+        borderRadius: "10px",
+        boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px"
+    }
+
+    const customStyle_1 = {
+        display: "grid",
+        alignItems: "center",
+        justifyItems: "center",
+        gridTemplateColumns: "1fr",
+        padding: "15px",
+        gridGap: "30px",
+        backgroundColor: "white",
+        width: "auto",
+        height: "800px",
+        margin: "15px",
+        justifySelf: "center",
+        alignSelf: "center",
+        borderRadius: "10px",
+        boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px"
+    }
+
+    for(let i = 0; i < usuario.alumnos.length; i++) {
+        if(user.email){
+            if (user.email === usuario.alumnos[i].email){
+                user_id = i;
+                connectSubject(user_id)
+                console.log("You have access requested")
+                break;
+            } else if (i === usuario.alumnos.length -1){
+                setTimeout( ()=> {
+                    navigate("/")
+                }, 300) 
+                console.log("You don't have access requested")
+            }
+        }
+    }
+    var img = {
+        borderRadius: "8px",
+        width:"100%"
+    }
+    var img_1 = {
+        borderRadius: "8px",
+        width:"100%",
+        maxHeight: "300px",
+        objectFit: "cover"
+    }
+    var box = {
+        backgroundColor: "#EAE8E8",
+        borderRadius: "12px",
+        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+        padding: "10px",
+        width: "80%",
+        display: "grid",
+        margin: "10px",
+        textDecoration: "none",
+        color: "black"
+    }
+    var content = {
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        alignItems: "center",
+        justifyItems: "center"
+    }
+    var center = {
+        alignItems: "center",
+        justifyItems: "center",
+        textAlign: "center"
+    }
+
+    function connectSubject(a){
+        let hasSubject = true;
+        for(let i = 0; i <= usuario.alumnos.length; i++){
+            console.log("You are?")
+            if (usuario.alumnos[a].access[i] === 0){
+                hasSubject = true
+                break;
+            } else if(i === usuario.alumnos.length){
+                hasSubject = false;
+                navigate("/")
+            }
+        }
+    }
+    return (
+        <div style={main}>
+            <div style={sidebar}></div>
+            <div>
+            <div style={customStyle_1}>
+                <img style={img_1} src={"https://i.imgur.com/QoeAo7L.jpg"}></img>
+                <h1>Identidad y Genética del Avivamiento</h1>
+                <p style={center}> El origen de nuestra identidad y la lucha contra Satanás y el pecado que intentan distorsionarla. Los principios del ADN espiritual que Esdras, Nehemías y otros hombres del AT usaron para originar los grandes avivamientos escriturales. La Biblia y Jesús como modelos únicos de lo que somos, lo que tenemos, lo que podemos ser (propósito); y a dónde vamos (destino)</p>
+            </div>
+            </div>
+        </div>
+    );
+  }
+  //<button style={button} onClick={googleLogin}>Ingresar con Google</button>
